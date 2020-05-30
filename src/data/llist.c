6f1fa8d717311aefaa7749eef8e834a552321995
Elias Mawa <elias@emawa.io>
2020-05-30 02:41:14 -0600

    implement linked list

---
+++ src/data/llist.c
@@ -0,0 +1,164 @@
+#include "llist.h"
+
+/*****************************************************
+ * ENUMERATIONS
+ *****************************************************/
+
+/*****************************************************
+ * TYPE DECLARATIONS
+ *****************************************************/
+
+/*****************************************************
+ * TYPE IMPLEMENTATIONS
+ *****************************************************/
+
+/*****************************************************
+ * FUNCTION DECLARATIONS
+ *****************************************************/
+
+/*****************************************************
+ * FUNCTION IMPLEMENTATIONS
+ *****************************************************/
+
+LLIST_T* llist_create(size_t size)
+{
+	LLIST_T* llist = malloc(sizeof(LLIST_T));
+	if(!llist) { return NULL; }
+
+	llist->head = NULL;
+	llist->tail = NULL;
+} // llist_create
+
+void llist_push_head(LLIST_T* llist, size_t key, void* item)
+{
+	LLIST_NODE_T* node = malloc(sizeof(LLIST_NODE_T));
+	if(!node) { return; }
+
+	node->back = llist->tail;
+	node->next = NULL;
+
+	node->key = key;
+	node->data = item;
+
+	if(llist->tail)
+	{
+		llist->tail->next = node;
+		llist->tail = node;
+	}
+	else
+	{
+		llist->tail = node;
+		llist->head = node;
+	}
+} // llist_push_head()
+
+void llist_push_tail(LLIST_T* llist, size_t key, void* item)
+{
+	LLIST_NODE_T* node = malloc(sizeof(LLIST_NODE_T));
+	if(!node) { return; }
+
+	node->next = llist->head;
+	node->back = NULL;
+	
+	node->key = key;
+	node->data = item;
+
+	if(llist->head)
+	{
+		llist->head->back = node;
+		llist->head = node;
+	}
+	else
+	{
+		llist->tail = node;
+		llist->head = node;
+	}
+} // llist_push_tail()
+
+LLIST_NODE_T* llist_pop(LLIST_T* llist, size_t key)
+{
+	LLIST_NODE_T* node = llist->head;
+
+	while(node && !(node->key == key))
+	{
+		node = node->next;
+	}
+
+	if(node->back)
+	{
+		node->back->next = node->next;
+	}
+	else
+	{
+		llist->head = node->next;
+	}
+
+	if(node->next)
+	{
+		node->next->back = node->back;
+	}
+	else
+	{
+		llist->tail = node->back;
+	}
+
+	node->back = NULL;
+	node->next = NULL;
+
+	return node;
+} // llist_pop()
+
+LLIST_NODE_T* llist_get(LLIST_T* llist, size_t key)
+{
+	LLIST_NODE_T* node = llist->head;
+
+	while(node && !(node->key == key))
+	{
+		node = node->next;
+	}
+
+	return node;
+} // llist_get()
+
+LLIST_NODE_T* llist_peek_head(LLIST_T* llist)
+{
+	return llist->head;
+} // llist_peek_head()
+
+LLIST_NODE_T* llist_peek_tail(LLIST_T* llist)
+{
+	return llist->tail;
+} // llist_peek_tail()
+
+LLIST_NODE_T* llist_cycle(LLIST_T* llist)
+{
+	LLIST_NODE_T* node = llist_peek_head(llist);
+	if(node)
+	{
+		llist_pop(llist, node->key);
+		llist_push_tail(llist, node->key, node->data);
+
+		free(node);
+
+		return llist_peek_tail(llist);
+	}
+	
+	return NULL;
+} // llist_cycle()
+
+void llist_free(LLIST_T* llist)
+{
+	if(llist)
+	{
+		llist_node_free(llist->head);
+		free(llist);
+		llist = NULL;
+	}
+} // llist_free()
+
+void llist_node_free(LLIST_NODE_T* node)
+{
+	if(node->next) { llist_node_free(node->next); }
+	free(node);
+	node = NULL;
+} // llist_node_free()
---
+++ src/data/llist.h
@@ -0,0 +1,117 @@
+#ifndef DATA_LLIST_H
+#define DATA_LLIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+/*****************************************************
+ * ENUMERATIONS
+ *****************************************************/
+
+/*****************************************************
+ * TYPE DECLARATIONS
+ *****************************************************/
+
+/** container for a linked list. */
+typedef struct LLIST LLIST_T;
+
+/** container for a linked list node. */
+typedef struct LLIST_NODE LLIST_NODE_T;
+
+/*****************************************************
+ * TYPE IMPLEMENTATIONS
+ *****************************************************/
+
+typedef struct LLIST {
+    LLIST_NODE_T* head; // index of front item in queue.
+	LLIST_NODE_T* tail; // index of rear item in queue.
+} LLIST_T;
+
+typedef struct LLIST_NODE {
+    LLIST_NODE_T* next; // index of front item in queue.
+	LLIST_NODE_T* back; // index of rear item in queue.
+	size_t key; // queue array.
+	void* data; // queue array.
+} LLIST_NODE_T;
+
+/*****************************************************
+ * FUNCTION DECLARATIONS
+ *****************************************************/
+
+/** 
+ * create an empty linked list.
+ * @param size length of linked list.
+ * @returns an empty linked list.
+ **/
+LLIST_T* llist_create(size_t size);
+
+/**
+ * insert an item into the front of a linked list.
+ * @param llist linked list to modify.
+ * @param item item to insert into linked list.
+ **/
+void llist_push_head(LLIST_T* llist, size_t key, void* item);
+
+/**
+ * insert an item into the back of a linked list.
+ * @param llist linked list to modify.
+ * @param item item to insert into linked list.
+ **/
+void llist_push_tail(LLIST_T* llist, size_t key, void* item);
+
+/**
+ * remove an item from a linked list.
+ * @param llist linked list to modify.
+ * @param key the key of the item to remove.
+ * @returns the item removed from the linked list (NULL if not found).
+ **/
+LLIST_NODE_T* llist_pop(LLIST_T* llist, size_t key);
+
+/**
+ * get an item from a linked list.
+ * @param llist linked list to modify.
+ * @param key the key of the item to get.
+ * @returns the item with the specified key (NULL if not found).
+ **/
+LLIST_NODE_T* llist_get(LLIST_T* llist, size_t key);
+
+/**
+ * get the head of a linked list.
+ * @param llist linked list to view.
+ * @returns the linked list head (NULL if not found).
+ **/
+LLIST_NODE_T* llist_peek_head(LLIST_T* llist);
+
+/**
+ * get the tail of a linked list.
+ * @param llist linked list to view.
+ * @returns the linked list tail (NULL if not found).
+ **/
+LLIST_NODE_T* llist_peek_tail(LLIST_T* llist);
+
+/**
+ * cycle an item into the back of a linked list.
+ * @param llist linked list to modify.
+ * @returns the item cycled to back of linked list (NULL if empty).
+ **/
+LLIST_NODE_T* llist_cycle(LLIST_T* llist);
+
+/**
+ * free a linked list from memory.
+ * @param llist linked list to free;
+ **/
+void llist_free(LLIST_T* llist);
+
+/**
+ * free a linked list node from memory.
+ * @param node node to free
+ **/
+void llist_node_free(LLIST_NODE_T* node);
+
+/*****************************************************
+ * FUNCTION IMPLEMENTATIONS
+ *****************************************************/
+
+#endif
